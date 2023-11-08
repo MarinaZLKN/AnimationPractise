e7364900e8f7ae6060e1acfeee0e5bb8e500e156
Marina <marina.zilkina@gmail.com>
2023-11-08 15:15:33 +0200

    custom icon

--- src/components/MapYandex.js
+++ src/components/MapYandex.js
@@ -1,11 +1,29 @@
-import { YMaps, Map } from '@pbe/react-yandex-maps';
+import {YMaps, Map, Placemark} from '@pbe/react-yandex-maps';
+import pic from '../images/447031.png';
 
 function MapYandex () {
+     const coordinates = [
+        [55.7558, 37.6176],
+        [55.7601, 37.6017],
+        [55.7650, 37.6082],
+     ];
 
     return(
         <YMaps>
-        <div>My awesome application with maps!</div>
-        <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+        <Map defaultState={{ center: [55.7558, 37.6176], zoom: 13 }} width="100%" height="400px">
+          {coordinates.map((coord, index) => (
+            <Placemark
+              key={index}
+              geometry={coord}
+              options={{
+                iconLayout: 'default#image',
+                iconImageHref: pic,
+                iconImageSize: [30, 30],
+                iconImageOffset: [-15, -30],
+              }}
+            />
+          ))}
+        </Map>
       </YMaps>
     );
 }
